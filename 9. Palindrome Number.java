2864980bc25f804862b8b185411af06f1d41b831
Ketan Goel <ketangoel@Ketans-MacBook-Pro.local>
2020-03-14 10:06:09 +0530

    best solution

--- 9. Palindrome Number.java
+++ 9. Palindrome Number.java
@@ -1,9 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+   if (x < 0 || (x != 0 && x % 10 == 0)) {
 			return false;
+		} else {
+			int reverse = 0;
+		int y = x;
+		while (x > reverse) {
+			reverse = reverse * 10 + x % 10;
+			x = x/10;
 		}
-		StringBuilder builder = new StringBuilder(Integer.toString(x));
-		return builder.toString().equals(builder.reverse().toString());
+		return (x==reverse || reverse/10 == x);
+		}
     }
 }

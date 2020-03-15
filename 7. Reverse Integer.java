c7ec82c2c7e0af1d5e079d1f731a9a38c00bb004
Ketan Goel <ketangoel@Ketans-MacBook-Pro.local>
2020-03-15 19:42:57 +0530

    this solution violates constraint and uses long, but solution becomes quite simple

--- 7. Reverse Integer.java
+++ 7. Reverse Integer.java
@@ -1,25 +1,17 @@
 class Solution {
     public int reverse(int x) {
            
-        int reverse = 0;
-		
-		while (x != 0) {
-			
-			int pop = x % 10;
-			x /= 10;
-			
-			if ((reverse > Integer.MAX_VALUE / 10) || (reverse == Integer.MAX_VALUE && pop > 7)) {
-				return 0;
-			}
-			if ((reverse < Integer.MIN_VALUE / 10) || (reverse == Integer.MIN_VALUE && pop < -8)) {
-				return 0;
-			}
-			reverse = reverse * 10 + pop;
-			
-			
-		}
+          long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
         
-        return reverse;
+        if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) {
+            return 0;
+        } else {
+            return (int)res;
+        }
 		
         
     }

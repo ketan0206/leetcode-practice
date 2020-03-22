6afb09b98e68bdd443fc0a4be5770521906cf5ad
Ketan Goel <ketangoel@Ketans-MacBook-Pro.local>
2020-03-22 10:35:57 +0530

    biweekly and weekly contest

---
+++ 1389. Create Target Array in the Given Order.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            list.add(index[i], nums[i]);
+            
+        }
+        
+        return list.stream().mapToInt(i->i).toArray();
+        
+    }
+}

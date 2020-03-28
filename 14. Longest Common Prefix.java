a04d7645bbbc231b24a03c247f0ff77fbf45c909
Ketan Goel <ketangoel@Ketans-MacBook-Pro.local>
2020-03-28 19:54:36 +0530

    optimised solution by horizontal scanning of common sequence

--- 14. Longest Common Prefix.java
+++ 14. Longest Common Prefix.java
@@ -1,25 +1,12 @@
     public String longestCommonPrefix(String[] strs) {
-       
-		StringBuilder s = new StringBuilder("");
-		if (strs.length == 0) {
-            return "";
-        }
-        if (strs.length == 1) {
-            return strs[0];
-        }
-		outerloop:
-		for (int n = 0 ; n < Integer.MAX_VALUE ; n++) {
-			for (int i = 0 ; i < strs.length - 1 ; i++) {
-				if (strs[i].length()>n && strs[i+1].length()>n) {
-					if (strs[i].charAt(n) != strs[i+1].charAt(n)) {
-						break outerloop;
-					}
-					
-				} else {
-					break outerloop;
-				}
-			}
-			s.append(strs[0].charAt(n));
-		}
-		return s.toString();	
+       public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+    String prefix = strs[0];
+    for (int i = 1; i < strs.length; i++)
+        while (strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length() - 1);
+            if (prefix.isEmpty()) return "";
+        }        
+    return prefix;	
+    }
     }

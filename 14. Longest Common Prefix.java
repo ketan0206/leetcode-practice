9f83e81b081ff80b0246af2873d4e1e4688e7ab0
Ketan Goel <ketangoel@Ketans-MacBook-Pro.local>
2020-03-28 19:55:49 +0530

    correction to previous version

--- 14. Longest Common Prefix.java
+++ 14. Longest Common Prefix.java
@@ -1,5 +1,4 @@
     public String longestCommonPrefix(String[] strs) {
-       public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) return "";
     String prefix = strs[0];
     for (int i = 1; i < strs.length; i++)
@@ -9,4 +8,4 @@
         }        
     return prefix;	
     }
-    }
+    

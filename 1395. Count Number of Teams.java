b27f26b1b67d1cf50ce66a3a5008bdff74fca307
Ketan Goel <ketangoel@Ketans-MacBook-Pro.local>
2020-03-29 11:10:28 +0530

    removed unnecessary list creation.

--- 1395. Count Number of Teams.java
+++ 1395. Count Number of Teams.java
@@ -1,27 +1,23 @@
 class Solution {
     public int numTeams(int[] rating) {
-        List<Integer> result = new ArrayList<>();
+        int result = 0;
 		for (int i = 0 ; i < rating.length ; i ++) {
 			for (int j = i+1 ; j < rating.length ; j++) {
 				if (j < rating.length && rating[i]>rating[j] ) {
 					for (int k = j+1 ; k < rating.length ; k++) {
 						if (k < rating.length && rating[j]>rating[k]) {
-							result.add(i);
-							result.add(j);
-							result.add(k);
+							result++;
 						}
 					}
 				} else if (j < rating.length && rating[i]<rating[j]) {
 					for (int k = j+1 ; k < rating.length ; k++) {
 						if (k < rating.length && rating[j]<rating[k]) {
-							result.add(i);
-							result.add(j);
-							result.add(k);
+							result++;
 						}
 					}
 				}
 			}
 		}
-		return result.size()/3;
+		return result;
     }
 }

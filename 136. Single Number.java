a71b9935e0e6d85d2b3fef665d42ca2acd5ac2c8
Ketan Goel <ketangoel@Ketans-MacBook-Pro.local>
2020-04-01 15:48:04 +0530

    using XOR for best sol.

--- 136. Single Number.java
+++ 136. Single Number.java
@@ -1,18 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if (nums.length==1) {
-           return nums[0]; 
-        }
-        Arrays.sort(nums);    
-		for (int i = 0 ; i < nums.length ; i++) {
-			if (i == 0 && nums[0]!=nums[i+1]) {
-				return nums[0];
-			} else if ((i == nums.length-1) && (nums[nums.length-2]!=nums[nums.length-1])) {
-				return nums[nums.length-1];
-			} else if (i>0 && i < nums.length-1 && (nums[i]!=nums[i-1]) && (nums[i]!=nums[i+1])) {
-				return nums[i];
-			}
-		}		
-		return 0;
+        int a = 0;
+    for (int i : nums) {
+      a ^= i;
+    }
+    return a;
     }
 }

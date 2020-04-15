6d047039e4fd95b7a911c11cae91eb3e666fa414
Ketan Goel <ketangoel@Ketans-MacBook-Pro.local>
2020-04-16 05:09:40 +0530

    nice catch

---
+++ 860. Lemonade Change.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+		int ten = 0;
+		for (int i : bills) {
+			if (i == 5) {
+				five++;
+			} else if (i == 10 ) {
+				five--;
+				ten++;
+			} else {
+				if (ten>0) {
+					five--;
+					ten--;
+				} else {
+					five = five -3;
+				}
+			}
+			if (five < 0 || ten < 0) {
+				return false;
+			}
+		}
+		return true;
+    }
+}

42e37c6a6dddc9496a1aa8e1fe2484feaff3a0ed
Ketan Goel <ketangoel@Ketans-MacBook-Pro.local>
2020-05-02 19:15:52 +0530

    not optimised, bad solution.

---
+++ 1365. How Many Numbers Are Smaller Than the Current Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        List<Integer> list =                            Arrays.stream(nums).boxed().collect(Collectors.toList());
+		Collections.sort(list);
+		
+		int[] result = new int[nums.length];
+		for (int i=0 ;i<nums.length; i++) {
+			result[i] = list.indexOf(Integer.valueOf(nums[i]));
+		}
+		return result; 
+    }
+}

2869d2918fc562bf0b97bcceef45ab20ee8f637a
ketan0206 <ketangoel2020@gmail.com>
2020-05-17 19:05:51 +0530

    May 16

---
+++ Odd Even Linked List.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/explore/featured/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3331/
+
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}

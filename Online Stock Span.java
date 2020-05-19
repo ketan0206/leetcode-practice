00838e72970a12824c14e711f1cff18b16a7d82a
ketan0206 <ketangoel2020@gmail.com>
2020-05-19 21:39:20 +0530

    May 19

---
+++ Online Stock Span.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3334/
+
+class StockSpanner {
+    Stack<Integer> prices, weights;
+
+    public StockSpanner() {
+        prices = new Stack();
+        weights = new Stack();
+    }
+
+    public int next(int price) {
+        int w = 1;
+        while (!prices.isEmpty() && prices.peek() <= price) {
+            prices.pop();
+            w += weights.pop();
+        }
+
+        prices.push(price);
+        weights.push(w);
+        return w;
+    }
+}

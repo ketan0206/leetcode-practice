a9394050faf9e4cb2a3f4362189d416e8ac80c58
ketan0206 <ketangoel2020@gmail.com>
2020-08-01 20:26:56 +0530

    better solution with regex.

--- August/Detect Capital.java
+++ August/Detect Capital.java
@@ -1,16 +1,21 @@
 // https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3409/
 
+/*
+
+	Approach 2: Regex  Intuition  Hey, if we want to do pattern matching, why don't we use Regular Expression (Regex)?
+	 Regex is a great way to match a given pattern to a string.  Algorithm  The pattern of case 1 in regex is  [ A − Z ] ∗ [A−Z]∗, 
+	 where  [ A − Z ] [A−Z] matches one char from 'A' to 'Z',  ∗ ∗ represents repeat the pattern before it at least 0 times. Therefore, 
+	 this pattern represents "All capital".  The pattern of case 2 in regex is  [ a − z ] ∗ [a−z]∗, where similarly,  
+	 [ a − z ] [a−z] matches one char from 'a' to 'z'. Therefore, this pattern represents "All not capital".  
+	 Similarly, the pattern of case 3 in regex is  [ A − Z ] [ a − z ] ∗ [A−Z][a−z]∗.  Take these three pattern together, 
+	 we have  [ A − Z ] ∗ ∣ [ a − z ] ∗ ∣ [ A − Z ] [ a − z ] ∗ [A−Z]∗∣[a−z]∗∣[A−Z][a−z]∗, where "|" represents "or".  
+	 Still, we can combine case 2 and case 3, and we get  . [ a − z ] ∗ .[a−z]∗, where "." can matches any char.  
+	 Therefore, the final pattern is  [ A − Z ] ∗ ∣ . [ a − z ] ∗ [A−Z]∗∣.[a−z]∗.
+
+*/
+
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if (word.toLowerCase().equals(word)) {
-			return true;
-		}
-		if (word.charAt(0) >=65 && word.charAt(0) <= 90 && word.substring(1).toLowerCase().equals(word.substring(1))) {
-			return true;
-		}
-		if (word.toUpperCase().equals(word)) {
-			return true;
-		}
-		return false;
+        return word.matches("[A-Z]*|.[a-z]*");
     }
 }

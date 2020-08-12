51ebd45d27052acc84b0d603dbc55d36e2d578dc
ketan0206 <ketangoel2020@gmail.com>
2020-08-12 16:31:09 +0530

    Better solution found in discuss section

--- August/Pascal's Triangle II.java
+++ August/Pascal's Triangle II.java
@@ -2,26 +2,13 @@
 
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> list = new ArrayList<>();
-		list.add(1);
-		if (rowIndex==0) {
-			
-		} else if (rowIndex==1) {
-			list.add(1);
-		} else {
-			
-			list.add(1);
-			
-			for (int l=1; l < rowIndex; l++) {
-				List<Integer> result = new ArrayList<>();
-				result.add(1);
-				for (int i = 0 ; i < list.size()-1; i++) {
-					result.add(list.get(i) + list.get(i+1));
-				}
-				result.add(1);
-				list = result;
-			}			
-		}	
-		return list;
+        Integer[] result = new Integer[rowIndex + 1];
+        Arrays.fill(result, 0)
+        result[0] = 1;
+        for (int i = 1; i <= rowIndex; i++)
+            for (int j = i; j > 0; j--)
+                result[j] = result[j] + result[j - 1];
+
+        return Arrays.asList(result);
     }
 }
